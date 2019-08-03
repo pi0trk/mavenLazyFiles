ab397ec8b010332b376b521f5134e84a73288462
pi0trk <35762402+pi0trk@users.noreply.github.com>
2019-08-03 19:32:36 +0200

    Maven bash files solo and zipped

---
+++ mvnFiles/mvnStartup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mvn archetype:generate -DgroupId=pl.karnas -Dversion=0.1 -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+cat mvnPomExample.xml > $1/pom.xml
+sed -i "s/<artifactId><\/artifactId>/<artifactId>$1<\/artifactId>/" $1/pom.xml
+echo "# $1" > $1/README.md
+cp .gitignore $1/
