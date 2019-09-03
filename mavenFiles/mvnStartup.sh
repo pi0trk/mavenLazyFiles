72b3959db85f472f26f44383522adb77474e7504
pi0trk <35762402+pi0trk@users.noreply.github.com>
2019-09-03 18:51:58 +0200

    mvnStartup.sh updated, POM name changed

--- mavenFiles/mvnStartup.sh
+++ mavenFiles/mvnStartup.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-mvn archetype:generate -DgroupId=pl.karnas -Dversion=0.1 -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
-cat mvnPomExample.xml > $1/pom.xml
-sed -i "s/<artifactId><\/artifactId>/<artifactId>$1<\/artifactId>/" $1/pom.xml
-sed -i "s/<name><\/name>/<name>$1<\/name>/" $1/pom.xml
-echo "# $1" > $1/README.md
-cp .gitignore $1/
-rm -f $1/src/test/java/pl/karnas/AppTest.java
+
+echo Please provide project name:
+read project_name
+echo Please provide project description:
+read project_description
+
+# Generate mvn project
+mvn archetype:generate -DgroupId=pl.karnas -Dversion=0.1 -DartifactId=$project_name -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+
+# Create pom.xml with $project_name as artifactId and name
+cat PomForMvn.xml > $project_name/pom.xml
+sed -i "s/<artifactId><\/artifactId>/<artifactId>$project_name<\/artifactId>/" $project_name/pom.xml
+sed -i "s/<name><\/name>/<name>$project_name<\/name>/" $project_name/pom.xml
+
+# Create README.md
+echo "# $project_name" > $project_name/README.md
+
+# Copy .gitignore to project
+cp .gitignore $project_name/
+
+# Remove AppTest
+rm -f $project_name/src/test/java/pl/karnas/AppTest.java
+
+# Create project description in main App
+sed -i "s/* Hello world\!/* $project_description \n *\n * @author pi0trk/" $project_name/src/main/java/pl/karnas/App.java
