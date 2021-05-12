ce2046c63f11c036e46cac04f1bcfcc3e5649bdf
pi0trk <35762402+pi0trk@users.noreply.github.com>
2021-05-12 18:36:16 +0200

    Update mvnStartup.sh

--- mavenFiles/mvnStartup.sh
+++ mavenFiles/mvnStartup.sh
@@ -6,7 +6,7 @@ echo Please provide project description:
 read project_description
 
 # Generate mvn project
-mvn archetype:generate -DgroupId=pl.karnas -Dversion=0.1 -DartifactId=$project_name -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+mvn archetype:generate -DgroupId=pl.pi0trk -Dversion=0.1 -DartifactId=$project_name -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 
 # Create pom.xml with $project_name as artifactId and name
 cat PomForMvn.xml > $project_name/pom.xml
@@ -20,7 +20,7 @@ echo "# $project_name" > $project_name/README.md
 cp .gitignore $project_name/
 
 # Remove AppTest
-rm -f $project_name/src/test/java/pl/karnas/AppTest.java
+rm -f $project_name/src/test/java/pl/pi0trk/AppTest.java
 
 # Create project description in main App
-sed -i "s/* Hello world\!/* $project_description \n *\n * @author pi0trk/" $project_name/src/main/java/pl/karnas/App.java
+sed -i "s/* Hello world\!/* $project_description \n *\n * @author pi0trk/" $project_name/src/main/java/pl/pi0trk/App.java
